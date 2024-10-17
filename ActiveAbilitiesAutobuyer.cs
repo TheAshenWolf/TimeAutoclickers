58c6c81e73bf49e4406c3036e139d65ee27df63f
TheAshenWolf <tawdevelopment@gmail.com>
2024-10-17 21:53:47 +0200

    Added ability autobuyer

---
+++ ActiveAbilitiesAutobuyer.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace TimeAutoclickers
+{
+    [HarmonyPatch(typeof(UIAbilityUnlocks)), HarmonyPatch("Refresh")]
+    public class ActiveAbilitiesAutobuyer()
+    {
+        public static void Postfix(UIAbilityUnlocks __instance)
+        {
+            // As buying an ability calls the Refresh, we don't need to loop this
+            if (__instance.unlockButton.interactable)
+            {
+                __instance.unlockButton.onClick.Invoke();
+            }
+        }
+    }
+}
