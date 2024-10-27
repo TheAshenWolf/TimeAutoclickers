1cb9f464f3918581a22f1a9a5040abe099426462
TheAshenWolf <tawdevelopment@gmail.com>
2024-10-27 20:19:15 +0100

    Updated version number
    (seems that I forgot to do that for a few versions already)

--- Constants.cs
+++ Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "com.github.TheAshenWolf.TimeAutoclickers";
         public const string PLUGIN_NAME = "TimeAutoclickers";
-        public const string PLUGIN_VERSION = "1.2.0";
+        public const string PLUGIN_VERSION = "1.5.0";
     }
 }
