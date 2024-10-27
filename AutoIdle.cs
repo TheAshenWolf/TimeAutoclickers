29851f4350c80f1dc70718a44b2e91df00fc6ebb
TheAshenWolf <tawdevelopment@gmail.com>
2024-10-27 19:53:34 +0100

    Guns now automaticall enter idle mode after prestige (tiny delay)

---
+++ AutoIdle.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TimeAutoclickers
+{
+    [HarmonyPatch(typeof(Arena)), HarmonyPatch("StartArena")]
+    public class AutoIdle
+    {
+        public static void Postfix()
+        {
+            UIButtonIdleMode[] buttons = Object.FindObjectsOfType<UIButtonIdleMode>();
+            foreach (UIButtonIdleMode button in buttons)
+            {
+                if ((int)Traverse.Create(button).Field("weaponMode").GetValue() == 1)
+                    continue;
+                Traverse.Create(button).Field("button").GetValue<Button>().onClick.Invoke();
+            }
+        }
+    }
+}
